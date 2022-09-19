19e5429c107f5c5015b8d999eb941a1684c4f8e9
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-09-19 10:32:15 +0300

    Initial Commit

---
+++ Task8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task8
+//
+//  Created by neoviso on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
